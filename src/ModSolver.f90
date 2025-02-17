8295e264a8c44b47f65301d9e2dace98d9431d6a
Alex Shane <adshane@umich.edu>
2025-02-17 12:44:07 -0500

    RK2 and Milstein solvers

--- src/ModSolver.f90
+++ src/ModSolver.f90
@@ -1,124 +1,73 @@
 module PT_ModSolver
-    use PT_ModConst, ONLY: fourpi
-    
-    use PT_ModKappa, ONLY: getK
-    use PT_ModShockPara, ONLY: getshock, getU, tmin_data, &
-        rmax_data, Mach, s, v_shock, V_sw_mod, r_shock, rMin, p0
+
+    use PT_ModRandom, ONLY: get_random_normal
+
+    use PT_ModFieldline, ONLY: get_sde_coeffs, get_sde_coeffs_milstein, &
+                               StratoFactor, nDim
 
     implicit none
+    SAVE
+    real :: Wk, Sk
 
 contains
-!==============================================================================
-subroutine predictor_corrector(r, p, t, dt, rNew, pNew)
-    ! Original predictor-corrector method
-    real, intent(in) :: r, p, t, dt
-    real, intent(out) :: rNew, pNew
+    !==============================================================================
+    subroutine milstein(X_I, Time, Timestep, Weight, Xnew_I)
+        real, intent(in) :: X_I(nDim), Time
+        real, intent(out) :: Timestep, Weight, Xnew_I(nDim)
 
-    real :: U, dUdr, K, dKdr, divU
-    real :: rn, xi
-    real :: rH, pH
+        real :: DriftCoeff(nDim), DiffCoeff(nDim), dDiffdX(nDim)
+        real :: RandomNormal
 
-    real, parameter :: OneThird = 1.0/3.0
-    
-    ! advance to the next position and momentum with
-    ! stochastic integration method
-    ! first step is the prediction step
-    call getU(r, t, U, dUdr)
-    call getK(r, p, K, dKdr)
+        call get_sde_coeffs_milstein(X_I, Timestep, Weight, DriftCoeff, DiffCoeff, dDiffdX)
+        
+        call get_random_normal(RandomNormal)
+        Wk = sqrt(Timestep)*RandomNormal
 
-    divU=2.d0*U/r + dUdr
+        Xnew_I = X_I + DriftCoeff * Timestep + DiffCoeff * Wk + &
+                 dDiffdX * (Wk**2 - Timestep)
 
-    call random_number(rn)
-    xi=-1.d0+2.d0*rn
+    end subroutine milstein
+    !==============================================================================
+    subroutine rk2_sde(X_I, Time, Timestep, Xnew_I)
+        ! Runge Kutta 2 Scheme for SDEs
+        ! Roberts 2012  "Modify the improved Euler scheme..."
+        real, intent(in) :: X_I(nDim), Time, Timestep
+        real, intent(out) :: Xnew_I(nDim)
 
-    rH = r + xi*sqrt(6.d0*K*dt) + U*dt + (dKdr+2.d0*K/r)*dt
-    pH = p*(1.d0 - OneThird*divU*dt)
+        real :: RandomNormal, RandomUniform
+        real :: K1_I(nDim), K2_I(nDim) ! runge-kutta coefficients
 
-    ! second step is the corrector step
-    call getU(rH,t,U,dUdr)
-    call getK(rH,pH,K,dKdr)
-    divU=2.d0*U/rH+dUdr
+        ! the same Wk and Sk needs to be used for both K1, K2
+        call get_random_normal(RandomNormal)
+        call random_number(RandomUniform)
+        
+        Sk = StratoFactor * sign(sqrt(Timestep), RandomUniform-0.5)
+        Wk = sqrt(Timestep)*RandomNormal - Sk
+        
+        call calculate_k(X_I, Time, TimeStep, K1_I)
 
-    rNew = r + xi*sqrt(6.d0*K*dt) + U*dt + (dKdr+2.d0*K/rH)*dt
-    pNew = p*(1.d0 - OneThird*divU*dt)
+        Wk = sqrt(Timestep)*RandomNormal + Sk
 
-end subroutine predictor_corrector
-!==============================================================================
-subroutine rk2_sde(r, p, t, dt, rNew, pNew)
-    ! Runge Kutta Scheme for SDEs
-    ! Stratonovich Version (S_k = 0)
-    ! Roberts 2012  "Modify the improved Euler scheme..."
-    real, intent(in) :: r, p, t, dt
-    real, intent(out) :: rNew, pNew
-    
-    real :: k1r, k1p, k2r, k2p ! runge-kutta coefficients
+        call calculate_k(X_I + K1_I, Time + Timestep, Timestep, K2_I)
 
-    call calculate_k(r, p, t, dt, k1r, k1p)
-    call calculate_k(r + k1r, p+k1p, t+dt, dt, k2r, k2p)
+        Xnew_I = X_I + 0.5 * (K1_I + K2_I)
 
-    rNew = r + 0.5 * (k1r + k2r)
-    pNew = p + 0.5 * (k1p + k2p)
+    end subroutine rk2_sde
+    !==============================================================================
+    subroutine calculate_k(X_I, Time, Timestep, K_I)
+        ! Calculate K1 and K2 for second order RK scheme for SDE
+        real, intent(in) :: X_I(nDim), Time, Timestep
+        real, intent(out) :: K_I(nDim)
 
-end subroutine rk2_sde
-!==============================================================================
-subroutine calculate_k(r, p, t, dt, kr, kp)
-    ! Calculate K1 and K2 for second order RK scheme for SDE
-    real, intent(in) :: r, p, t, dt
-    real, intent(out) :: kr, kp
+        integer :: i
+        real :: DriftCoeff(nDim), DiffCoeff(nDim) !, pDiff
 
-    real :: rn1, rn2, wk1, wk2
-    real :: rDrift, rDiff, pDrift, pDiff
+        call get_sde_coeffs(X_I, Time, Timestep, DriftCoeff, DiffCoeff)
+        do i = 1, nDim
+            K_I(i) = Timestep * DriftCoeff(i) + Wk * DiffCoeff(i)
+        end do
 
-    call get_sde_coeffs(r, p, t, rDrift, rDiff, pDrift, pDiff)
-
-    call get_random_normal(rn1)
-    call get_random_normal(rn2)
-    wk1 = sqrt(dt)*rn1
-    wk2 = sqrt(dt)*rn2
-    
-    kr = dt * rDrift + wk1 * rDiff   ! different wks?
-    kp = dt * pDrift + wk2 * pDiff
-
-end subroutine calculate_k
-!==============================================================================
-subroutine get_sde_coeffs(r, p, t, rDrift, rDiff, pDrift, pDiff)
-    ! Get drift and diffusion coefficients for the Stratonovich
-    ! representation - see Pei 2010 for Ito coefficients
-    real, intent(in) :: r, p, t
-    real, intent(out) :: rDrift, rDiff, pDrift, pDiff
-
-    real :: U, dUdr, K, dKdr
-
-    call getU(r, t, U, dUdr)
-    call getK(r, p, K, dKdr)
-
-    rDrift = U + (2.0*K/r) + 0.5*dKdr
-    rDiff = sqrt(2.0*K)
-
-    pDrift = -p/3.0 * (2*U/r + dUdr)
-    pDiff = 0.0
-
-end subroutine get_sde_coeffs
-!==============================================================================
-subroutine get_random_normal(rn)
-    ! returns random number sampled from normal distribution
-    ! with mean = 0 and std = 1
-
-    real, intent(out) :: rn
-    real :: ru1, ru2
-
-    ! uniform random numbers over [0,1)
-    call random_number(ru1)
-    call random_number(ru2)
-
-    ! redistribute to (0, 1] to avoid 0
-    ru1 = 1 - ru1
-    ru2 = 1 - ru2
-    
-    ! Box-Muller transformation
-    rn = sqrt(-2*log(ru1))*cos(0.5*fourpi*ru2)
-
-end subroutine get_random_normal
-!==============================================================================
+    end subroutine calculate_k
+    !==============================================================================
 end module PT_ModSolver
 !==============================================================================
